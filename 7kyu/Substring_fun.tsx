87c8cb5e231b9cc14e422e5bb0af11c400696b0d
Fyodor <jeelcopper@gmail.com>
2025-04-29 16:36:52 +0300

    Добавлены решения задач

---
+++ 7kyu/Substring_fun.tsx
@@ -0,0 +1,25 @@
+/*
+Complete the function that takes an array of words.
+
+You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+
+For example:
+
+["yoda", "best", "has"]  -->  "yes"
+  ^        ^        ^
+  n=0     n=1     n=2
+Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
+
+*/
+
+function nthChar(words: string[]) {
+  if (words) {
+    let result: string[] = [];
+    let j = 0;
+    for (let i = 0; i < words.length; i++) {
+      result.push(words[i][j]);
+      j++;
+    }
+    return result.join("");
+  }
+}
